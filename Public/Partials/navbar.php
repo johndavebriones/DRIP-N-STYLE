0536f64d6e7333a6860689df28ab091e9e05b5ae
johndavebriones <johndavealdaybriones009@gmail.com>
2025-10-25 18:08:03 +0800

    Modified UI for guest/customers

--- Public/Partials/navbar.php
+++ Public/Partials/navbar.php
@@ -1,32 +1,36 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-  session_start();
-}
-?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand text-warning" href="index.php">Drip N' Style</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+    <a class="navbar-brand text-warning" href="../index.php">Drip N' Style</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-        <?php if(isset($_SESSION['user_id'])): ?>
-          <li class="nav-item"><a class="nav-link" href="cart.php">Cart</a></li>
+      <ul class="navbar-nav ms-auto align-items-center">
+        <li class="nav-item"><a class="nav-link" href="../index.php">Home</a></li>
+
+        <?php if (isset($_SESSION['user_id'])): ?>
+          <li class="nav-item"><a class="nav-link" href="../cart.php">Cart</a></li>
+
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+               data-bs-toggle="dropdown" aria-expanded="false">
               <?= htmlspecialchars($_SESSION['user_name']); ?>
             </a>
-            <ul class="dropdown-menu dropdown-menu-end">
-              <li><a class="dropdown-item" href="profile.php">Profile</a></li>
-              <li><a class="dropdown-item" href="../app/controllers/AuthController.php?action=logout">Logout</a></li>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+              <li><a class="dropdown-item" href="../profile.php">Profile</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item text-danger" href="../../App/Controllers/AuthController.php?action=logout">Logout</a></li>
             </ul>
           </li>
         <?php else: ?>
-          <li class="nav-item"><a class="nav-link" href="LoginPage.php">Log in</a></li>
+          <li class="nav-item"><a class="nav-link" href="../LoginPage.php">Login</a></li>
         <?php endif; ?>
       </ul>
     </div>
   </div>
 </nav>
+
+<!-- ✅ REQUIRED Bootstrap JS Bundle (includes Popper) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
