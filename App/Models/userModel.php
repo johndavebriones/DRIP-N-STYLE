64fd0b0f48a85a47a51ca83b1376d9ebac3a5f17
johndavebriones <johndavealdaybriones009@gmail.com>
2025-11-01 12:06:25 +0800

    Added User model

---
+++ App/Models/userModel.php
@@ -0,0 +1,13 @@
+<?php
+class UserModel {
+    public $user_id;
+    public $name;
+    public $email;
+    public $password;
+    public $role;
+    public $status;
+    public $date_created;
+    public $reset_token;
+    public $token_expiry;
+    public $contact_number;
+}
