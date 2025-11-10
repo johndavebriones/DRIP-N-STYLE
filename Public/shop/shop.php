baa264d1b675973871ba13ce490596f0297459fb
johndavebriones <johndavealdaybriones009@gmail.com>
2025-11-10 17:38:31 +0800

    minor changes on cart

--- Public/shop/shop.php
+++ Public/shop/shop.php
@@ -1,10 +1,5 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-require_once __DIR__ . '/../../App/Helpers/SessionHelper.php';
 require_once __DIR__ . '/../../App/Controllers/ShopController.php';
-SessionHelper::preventCache();
 
 $shop = new ShopController();
 $search = $_GET['search'] ?? '';
