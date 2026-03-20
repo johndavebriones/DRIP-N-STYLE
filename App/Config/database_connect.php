82e1803dfdb1f6362124314b21e2435fb7050d1e
johndavebriones <johndavealdaybriones009@gmail.com>
2026-03-20 21:43:57 +0800

    updated connection and gitignore

--- App/Config/database_connect.php
+++ App/Config/database_connect.php
@@ -1,15 +1,24 @@
 <?php
 class Database {
-    private $servername = "localhost";
+    private $servername = "127.0.0.1";
     private $username = "root";
     private $password = "";
     private $dbname = "dripnstyle";
+    private $port = 3306;
 
     public function connect() {
-        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        $conn = new mysqli(
+            $this->servername,
+            $this->username,
+            $this->password,
+            $this->dbname,
+            $this->port
+        );
+
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
+
         return $conn;
     }
 }
